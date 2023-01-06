8bf822e9f33e09606f80c8b44a2b8102d162dc70
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-01-06 02:43:26 +0000

    Link JavaScript with html

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-var mylibrary = [];
+// var mylibrary = [];
 
-// constructor 
-function Book (title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = false;
-    this.info = ()=>{
+// // constructor 
+// function Book (title, author, pages, read){
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = false;
+//     this.info = ()=>{
 
-        if(this.read == false){
-            this.read = "Not read yet";
-        }
-        else{
-            this.read = "Has been read"
-        }
+//         if(this.read == false){
+//             this.read = "Not read yet";
+//         }
+//         else{
+//             this.read = "Has been read"
+//         }
 
-        return `${this.title},${this.author}, ${this.pages}, ${this.read}`
-    }
-}
+//         return `${this.title},${this.author}, ${this.pages}, ${this.read}`
+//     }
+// }
 
-function addBookToLibrary(title, author, pages, read) {
-    // do stuff here
-    title = new Book(title, author, pages, read);
-    mylibrary.push(title);
-}
+// function addBookToLibrary(title, author, pages, read) {
+//     // do stuff here
+//     title = new Book(title, author, pages, read);
+//     mylibrary.push(title);
+// }
 
-addBookToLibrary("Winter in Africa", "Boakia", 8, true);
+// addBookToLibrary("Winter in Africa", "Boakia", 8, true);
 
-console.log(mylibrary)
+// console.log(mylibrary)
 
+
+console.log("Working");
