f854c8024d67941f89af42755f6db8a96a37f50a
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-01-06 00:48:31 +0000

    Fist commit with html, css, and javascript files created

---
+++ script.js
@@ -0,0 +1,31 @@
+var mylibrary = [];
+
+// constructor 
+function Book (title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = false;
+    this.info = ()=>{
+
+        if(this.read == false){
+            this.read = "Not read yet";
+        }
+        else{
+            this.read = "Has been read"
+        }
+
+        return `${this.title},${this.author}, ${this.pages}, ${this.read}`
+    }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    // do stuff here
+    title = new Book(title, author, pages, read);
+    mylibrary.push(title);
+}
+
+addBookToLibrary("Winter in Africa", "Boakia", 8, true);
+
+console.log(mylibrary)
+
