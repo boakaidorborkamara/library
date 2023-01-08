06e7cccaaefbd3c07e989ca739494b2bbbfff96b
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-01-08 22:45:26 +0000

    Refactor and rename some functions

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function Book (title, author, pages, read){
     }
 }
 
-// Get info for new book 
+// Get info for new book from frontend
 function getNewBookInfo(){
 
     let book_name = document.querySelector("#book-name");
@@ -30,27 +30,29 @@ function getNewBookInfo(){
 
 }
 
-// create and add new book to mylibrary array or database 
-function addBookToLibrary() {
+// create and add new book to database array
+function addBookToDatabaseArray() {
 
     // information needed to create new book 
     let new_book_info = getNewBookInfo();
 
-    // create new book 
+    // create new book based on the new book info
     let new_book =  new Book(new_book_info.name, new_book_info.author, new_book_info.pages, new_book_info.status);
 
-    // add to array db 
+    // add to new book to db array 
     mylibrary.push(new_book);
 
     //display books from db
-    displayBook();
+    displayBookFromDatabase();
+
+    deleteBook();
 
     return 0;
 
 }
 
 //display books on the html page
-function displayBook(){
+function displayBookFromDatabase(){
     //get table element
     console.log("Displaying books fnx");
 
@@ -64,11 +66,11 @@ function displayBook(){
         console.log(index);
 
         html = `
-            <tr>
+            <tr id=table-row-${index}>
                 <td>${ele.title}</td>
                 <td>${ele.author}</td>
                 <td><button id=${index}>${ele.read}</button></td>
-                <td><button id=${index}>Delete</button></td>
+                <td><button class="delete-btn" id=${index}>Delete</button></td>
             </tr>
         `;   
 
@@ -76,20 +78,47 @@ function displayBook(){
 
     
     table_body.insertAdjacentHTML("beforeend", html);
+    console.log(mylibrary.length, "in display book fnx")
 
     return 0;
 }
 
 //delete an existing book
 function deleteBook(){
+    let delete_btns = document.querySelectorAll(".delete-btn");
+    // console.log(delete_btns);
 
+    
+    delete_btns.forEach((ele)=>{
+        // console.log(ele);
+        ele.addEventListener("click",(e)=>{
+
+            //get the index for the book which the currently clicked delete btn is attached to
+            let book_index = e.target.getAttribute("id");
+
+            //remove the book that is being targeted from myLibrary array / db
+            console.log("deleting from db")
+            mylibrary.splice(book_index,1);
+            console.log("done deleting")
+            console.log(mylibrary);
+
+            let targeted_table_row = document.getElementById(`table-row-${book_index}`);
+            console.log(targeted_table_row);
+            // targeted_table_row.remove();
+            
+            // console.log(mylibrary.length);
+
+            //update the dom with changes made in the db
+            // displayBook();
+        })
+    })
 }
 
 
 add_new_btn.addEventListener("click",(e)=>{
     console.log("Working")
     e.preventDefault();
-    addBookToLibrary();
+    addBookToDatabaseArray
 })
 
 
